28560ca82349d2931fdeb88f50b3899e9ed46cbe
A-m-Tawfik <a.mohammed.tawfik@gmail.com>
2019-10-16 13:52:47 +0200

    initial repository commit.

---
+++ stats.h
@@ -0,0 +1,27 @@
+/******************************************************************************
+ * Copyright (C) 2017 by Alex Fosdick - University of Colorado
+ *
+ * Redistribution, modification or use of this software in source or binary
+ * forms is permitted as long as the files maintain this copyright. Users are 
+ * permitted to modify this and use it to learn about the field of embedded
+ * software. Alex Fosdick and the University of Colorado are not liable for any
+ * misuse of this material. 
+ *
+ *****************************************************************************/
+/**
+ * @file  stats.h 
+ * @brief this file holds the interfaces of the stats SWC
+ *
+ * 
+ * @author Ahmed Tawfik
+ * @date   16/10/2019
+ *
+ */
+#ifndef __STATS_H__
+#define __STATS_H__
+
+
+
+
+
+#endif /* __STATS_H__ */
